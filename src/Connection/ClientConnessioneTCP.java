f0b88797a9f4899d6966fb919c93b3c597dc5763
Jesus Levano <marcelolevano23@gmail.com>
2018-02-01 21:17:34 +0100

    Aggiungo le due classi di supporto e le due classi principali

--- src/Connection/ClientConnessioneTCP.java
+++ src/Connection/ClientConnessioneTCP.java
@@ -3,18 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Connection;
 
 /**
  *
  * @author jesus
  */
-public class ProvaGitLev {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ClientConnessioneTCP {
     
 }
