7667656c80b2b199994aa039416f06fbb06887f6
Jesus Levano <marcelolevano23@gmail.com>
2018-02-01 19:49:28 +0100

    Aggiungo un ciclo while al Clien così da continuare a comunicare con il server

--- src/Connection/ClientConnessioneTCP.java
+++ src/Connection/ClientConnessioneTCP.java
@@ -30,45 +30,52 @@ public class ClientConnessioneTCP {
         int port = 2000;
         String messaggioOutput, messaggioInput;
         //apertura della connessione al server sulla porta specificata
-        try{
-            connection = new Socket(serverAddress, port);
-            System.out.println("Connessione aperta");
-            // Creo input e output per streams orientati ai byte
-            BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
-            BufferedReader inputClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            PrintStream outputClient = new PrintStream(connection.getOutputStream());
-            
-            System.out.println("Che voi scrivere al server?");
-            messaggioOutput = tastiera.readLine();
-            outputClient.println(messaggioOutput);
-            outputClient.flush();
-            
-            messaggioInput = inputClient.readLine();
-            System.out.println(messaggioInput);
-            
-        }
-        catch(ConnectException e){
-            System.err.println("Server non disponibile!\n" + e);
-        }
-        catch(UnknownHostException e1){
-            System.err.println("Errore DNS!\n" + e1);
-        }
+        boolean continua = true;
+        
+        while(continua) {
+            try{
+                connection = new Socket(serverAddress, port);
+                System.out.println("Connessione aperta");
+                // Creo input e output per streams orientati ai byte
+                BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
+                BufferedReader inputClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                PrintStream outputClient = new PrintStream(connection.getOutputStream());
 
-        catch(IOException e2){
-            System.err.println(e2);
-        }
-
-        //chiusura della connnessione
-        finally{
-                try {
-            if (connection!=null)
-                {
-                    connection.close();
-                    System.out.println("Connessione chiusa!");
+                System.out.println("Che voi scrivere al server?");
+                messaggioOutput = tastiera.readLine();
+                outputClient.println(messaggioOutput);
+                outputClient.flush();
+                
+                messaggioInput = inputClient.readLine();
+                System.out.println(messaggioInput);
+                if(messaggioInput.equals("Ciao ciao!")) {
+                    continua = false;
                 }
+
             }
-            catch(IOException e){
-                System.err.println("Errore nella chiusura della connessione!");
+            catch(ConnectException e){
+                System.err.println("Server non disponibile!\n" + e);
+            }
+            catch(UnknownHostException e1){
+                System.err.println("Errore DNS!\n" + e1);
+            }
+
+            catch(IOException e2){
+                System.err.println(e2);
+            }
+
+            //chiusura della connnessione
+            finally{
+                    try {
+                if (connection!=null)
+                    {
+                        connection.close();
+                        System.out.println("Connessione chiusa!");
+                    }
+                }
+                catch(IOException e){
+                    System.err.println("Errore nella chiusura della connessione!");
+                }
             }
         }
     }
