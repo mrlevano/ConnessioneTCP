e921b74d7a826507ed42cf13fa9f3c989f519d3e
Jesus Levano <marcelolevano23@gmail.com>
2018-02-01 22:09:21 +0100

    Strutturati gli attributi e i metodi delle due classi di appoggio

--- src/Connection/ClientConnessioneTCP.java
+++ src/Connection/ClientConnessioneTCP.java
@@ -5,10 +5,54 @@
  */
 package Connection;
 
+import java.net.Socket;
+
 /**
  *
  * @author jesus
  */
 public class ClientConnessioneTCP {
     
+    /**
+     * Porta nella quale il server è in ascolto
+     */
+    private final int porta;
+    
+    /**
+     * Indirizzo del server
+     */
+    private final String indirizzoS;
+    
+    /**
+     * Costruttore della classe ClientConnessionTCP
+     */
+    public ClientConnessioneTCP() {
+        porta = 2000;
+        indirizzoS = "localhost";
+    }
+    
+    /**
+     * Metodo che avvia una connessione e una comunicazione con il server
+     */
+    public void avviaConnessione() {
+        Socket connessione = null;
+        
+    }
+    
+    /**
+     * Metodo che ottiene la stringa che il server manda come risposta alla richiesta del client
+     * @param connessione Oggetto socket che server che contiene gli InputStream e OutputStream
+     */
+    public void comunicaS(Socket connessione) {
+        String messaggioOutput, messaggioInput;
+        
+    }
+    
+    /**
+     * Metodo che chiude la connessione con il server
+     * @param connessione Oggetto socket che server contenente il metodo .close()
+     */
+    public void chiudiConnessione(Socket connessione) {
+        
+    }
 }
--- src/Connection/ServerConnessioneTCP.java
+++ src/Connection/ServerConnessioneTCP.java
@@ -5,10 +5,50 @@
  */
 package Connection;
 
+
+import java.net.Socket;
+
 /**
  *
  * @author jesus
  */
 public class ServerConnessioneTCP {
+    
+    /**
+     * Porta nella quale il server è in ascolto
+     */
+    private final int porta;
+    
+    /**
+     * Costruttore della classe ServerConnessioneTCP
+     */
+    public ServerConnessioneTCP() {
+        porta = 2000;
+    }
+    
+    /**
+     * Metodo che avvia il server e lo mette in ascolto sulla sua porta
+     */
+    public void avviaServer() {
+        Socket connessione = null;
+        
+    }
+    
+    /**
+     * Metodo che legge la stringa inviata da un client e ne elabora una risposta
+     * @param connessione Oggetto socket che server che contiene gli InputStream e OutputStream
+     */
+    public void rispondi(Socket connessione) {
+        String messaggioOutput, messaggioInput;
+        
+    }
+    
+    /**
+     * Metodo che chiude la connessione con il server
+     * @param connessione Oggetto socket che server contenente il metodo .close()
+     */
+    public void spegniServer(Socket connessione) {
+        
+    }
     
 }
