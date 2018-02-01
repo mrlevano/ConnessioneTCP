782c0347fa8cd022e4cbf399403c86b0fbb264c9
Jesus Levano <marcelolevano23@gmail.com>
2018-02-01 22:33:09 +0100

    Completato la classe d'appoggio per il client

--- src/Connection/ClientConnessioneTCP.java
+++ src/Connection/ClientConnessioneTCP.java
@@ -5,7 +5,13 @@
  */
 package Connection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  *
@@ -35,24 +41,74 @@ public class ClientConnessioneTCP {
      * Metodo che avvia una connessione e una comunicazione con il server
      */
     public void avviaConnessione() {
-        Socket connessione = null;
+        Socket connection = null;
+        boolean continua = true;
+        try {
+            connection = new Socket(indirizzoS, porta);
+            
+            while(continua) { // Conntinuo a ripetere fino a quando .comunicaS() ritorna false
+                continua = comunicaS(connection);
+            }
+            
+        } catch(ConnectException e){
+            System.err.println("Server non disponibile!\n" + e.getMessage());
+        } catch(UnknownHostException e1){
+            System.err.println("Errore DNS!\n" + e1.getMessage());
+        } catch (IOException e2) {
+            System.err.println("Errore : " + e2.getMessage());
+        } finally {
+            chiudiConnessione(connection);
+        }
         
     }
     
     /**
      * Metodo che ottiene la stringa che il server manda come risposta alla richiesta del client
-     * @param connessione Oggetto socket che server che contiene gli InputStream e OutputStream
+     * @param connection Oggetto socket che server che contiene gli InputStream e OutputStream
+     * @return Booleana che indica se si deve continuare a scrivere al server oppure chiudere la connessione
      */
-    public void comunicaS(Socket connessione) {
+    public boolean comunicaS(Socket connection) {
         String messaggioOutput, messaggioInput;
+        boolean continua = false;
         
+        try {
+            BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader inputClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            PrintStream outputClient = new PrintStream(connection.getOutputStream());
+            
+            // Input da tastiera per il messaggio da mandare al server
+            System.out.println("Che voi scrivere al server?");
+            messaggioOutput = tastiera.readLine();
+            // Invio il messaggio al server
+            outputClient.println(messaggioOutput);
+            outputClient.flush();
+            
+            // Ricevo la risposta del server
+            messaggioInput = inputClient.readLine();
+            System.out.println(messaggioInput);
+            
+            if(messaggioInput.equals("Ciao ciao!")) { // Se il sever risponde ciao ciao allora si ritorna falso
+                continua = false;
+            }
+            
+        } catch (IOException ex) {
+            System.err.println("Errore : " + ex.getMessage());
+        }
+        
+        return continua;
     }
     
     /**
      * Metodo che chiude la connessione con il server
-     * @param connessione Oggetto socket che server contenente il metodo .close()
+     * @param connection Oggetto socket che server contenente il metodo .close()
      */
-    public void chiudiConnessione(Socket connessione) {
-        
+    public void chiudiConnessione(Socket connection) {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (IOException ex) {
+                System.err.println("Errore durante la chiusura: " + ex.getMessage());
+            }
+        }
     }
 }
